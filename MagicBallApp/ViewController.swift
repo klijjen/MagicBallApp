344205c665e9b44ffa85f5ba5341c17f3474751d
Zahar Litvinchuk <97597336+raynelz@users.noreply.github.com>
2023-08-08 20:14:48 +0300

    final commit

--- MagicBallApp/ViewController.swift
+++ MagicBallApp/ViewController.swift
@@ -8,11 +8,5 @@
 import UIKit
 
 class ViewController: UIViewController {
-    @IBOutlet var imageView: UIImageView!
-
     var ballArray = [UIImage(imageLiteralResourceName: "ball1"), UIImage(imageLiteralResourceName: "ball2"), UIImage(imageLiteralResourceName: "ball3"), UIImage(imageLiteralResourceName: "ball4"), UIImage(imageLiteralResourceName: "ball5")]
-
-    @IBAction func askButtonPressed(_ sender: UIButton) {
-        imageView.image = ballArray.randomElement()
-    }
 }
