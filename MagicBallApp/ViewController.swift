f7f31ce740f99dba6211e7fdc824afd9eac7babb
klijjen <luydofa_04@icloud.com>
2023-10-10 23:04:53 +0300

    lalaala

--- MagicBallApp/ViewController.swift
+++ MagicBallApp/ViewController.swift
@@ -1,12 +1,18 @@
-//
-//  ViewController.swift
-//  MagicBallApp
-//
-//  Created by Захар Литвинчук on 08.08.2023.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
     var ballArray = [UIImage(imageLiteralResourceName: "ball1"), UIImage(imageLiteralResourceName: "ball2"), UIImage(imageLiteralResourceName: "ball3"), UIImage(imageLiteralResourceName: "ball4"), UIImage(imageLiteralResourceName: "ball5")]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        imageView.image = ballArray[2]
+    }
+
+    @IBAction func askButtonPressed(_ sender: UIButton) {
+        imageView.image = ballArray[Int.random(in: 0...4)]
+    }
 }
